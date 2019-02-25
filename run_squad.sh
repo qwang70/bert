48fefcbff9253f696cd01cfc99098ed552fcb26b
Qiwen Wang <qiwen.wang@163.com>
2019-02-25 04:46:56 +0000

    add tuned result

---
+++ run_squad.sh
@@ -0,0 +1,20 @@
+BERT_LARGE_DIR=$HOME'/bert/BERT_Large_uncased_L-24_H-1024_A-16'
+BERT_BASE_DIR=$HOME'/bert/BERT_Base_uncased_L-12_H-768_A-12'
+BERT_MODEL_DIR=$BERT_BASE_DIR
+#CUDA_VISIBLE_DEVICES=0 
+python run_squad.py \
+  --vocab_file=$BERT_MODEL_DIR/vocab.txt \
+  --bert_config_file=$BERT_MODEL_DIR/bert_config.json \
+  --init_checkpoint=$BERT_MODEL_DIR/bert_model.ckpt \
+  --do_train=True \
+  --train_file=$SQUAD_DIR/train-v2.0.json \
+  --do_predict=True \
+  --predict_file=$SQUAD_DIR/dev-v2.0.json \
+  --train_batch_size=6 \
+  --learning_rate=3e-5 \
+  --num_train_epochs=2.0 \
+  --max_seq_length=384 \
+  --doc_stride=128 \
+  --output_dir=outputs/squad/ \
+  --use_tpu=False\
+  --version_2_with_negative=True
