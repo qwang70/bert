27ef15a70a036d8995ab8d4ba0f3e0b499e0c28f
qwang70 <qiwen.wang@163.com>
2019-02-28 15:05:25 -0800

    viz

---
+++ script/dev_viz.py
@@ -0,0 +1,7 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set(style="white", palette="muted", color_codes=True)
+
+iterrogative_word = ['which', 'what', 'whose', 'who', 'whom', 'where', 'when', 'how', 'why', 'is']
--- script/stats_viz.py
+++ script/stats_viz.py
@@ -17,11 +17,11 @@ def training_stats():
     question_global_info = []
     question_local_info = {}
     for w in iterrogative_word:
-        question_local_info[w] = []
+        question_local_info[w] = 0
     for w in yn_qa:
-        question_local_info[w] = []
+        question_local_info[w] = 0
     for w in other_qa:
-        question_local_info[w] = []
+        question_local_info[w] = 0
     answer_length = []
     
     # iter training data stats
@@ -43,34 +43,51 @@ def training_stats():
                     for w in iterrogative_word:
                         if w in question:
                             question_word_count += 1
-                            question_local_info[w].append((len(question.split()), is_impossible))
+                            question_local_info[w] += 1
                     if question_word_count == 0:
                         if question.split()[0] in yn_qa: 
                             question_word_count += 1
-                            question_local_info[question.split()[0]].append((len(question.split()), is_impossible))
+                            question_local_info[question.split()[0]] += 1
                             
                     if question_word_count == 0:
                         for w in other_qa:
                             if w in question:
                                 question_word_count += 1
-                                question_local_info[w].append((len(question.split()), is_impossible))
-    """
-                    if question_word_count == 0:
-                        print(question)
-    """
+                                question_local_info[w] += 1
+                    if not is_impossible:
+                        answers = qa['answers']
+                        ans_len = []
+                        for answer in answers:
+                            ans_len.append(len(answer["text"].split()))
+                        answer_length.append(sum(ans_len)/len(ans_len))
+                    
     # viz
+    fig, ax = plt.subplots()
     context_dist = sns.distplot(context_length, kde=False, color="b", axlabel="context length")
     context_dist.figure.savefig("../outputs/training_viz/context_dist.png")
 
     fig, ax = plt.subplots()
+    context_dist = sns.distplot(answer_length, kde=False, color="b", axlabel="word length of answers")
+    plt.xlim(right=20)
+    context_dist.figure.savefig("../outputs/training_viz/ans_len_dist.png")
+
+    fig, ax = plt.subplots()
     possible_qa = list(map(lambda x: x[0], list(filter(lambda x: x[1] is True, question_global_info))))
     impossible_qa = list(map(lambda x: x[0], list(filter(lambda x: x[1] is False, question_global_info))))
-    qa_dist = sns.distplot(possible_qa, ax = ax, label = "possible to answer", axlabel = 'question length')
-    qa_dist = sns.distplot(impossible_qa, ax = ax, label = "impossible to answer", axlabel = 'question length')
+    qa_dist = sns.distplot(possible_qa, hist=False, ax = ax, label = "possible to answer", axlabel = 'question length')
+    qa_dist = sns.distplot(impossible_qa, hist=False,ax = ax, label = "impossible to answer", axlabel = 'question length')
+    ax.legend()
     qa_dist.figure.savefig("../outputs/training_viz/qa_dist.png")
-#    seaborn.kdeplot(df['col1'], ax=ax)
-#    seaborn.kdeplot(df['col2'], ax=ax)
 
+    fig, ax = plt.subplots()
+    lists = sorted(question_local_info.items(), key=lambda x: x[1], reverse=True)[:10] # sorted by key, return a list of tuples
+    x, y = zip(*lists) # unpack a list of pairs into two tuples
+    plt.scatter(x, y)
+    for i,j in zip(x,y):
+        ax.annotate(str(j),xy=(i,j+0.5))
+    plt.xlabel("iterrogative_word")
+    plt.ylabel("frequency")
+    fig.savefig("../outputs/training_viz/qa_questiontype_dist.png")
 
 if __name__ == '__main__':
     training_stats()
